5f231e345d972918a1c56e4a68ba9f7d382841a4
Stephen Johnson <steve@steveasleep.com>
2013-11-28 16:27:31 -0800

    Update documentation

---
+++ _sources/examples/imgur.txt
@@ -0,0 +1,11 @@
+Uploading to Imgur
+==================
+
+.. raw:: html
+
+    <div class="literally imgur"><canvas></canvas></div>
+
+        <a data-action="upload-to-imgur"
+            href="https://api.imgur.com/oauth2/authorize?response_type=token&client_id=4955b0f56a747be">
+            Upload to Imgur
+        </a>
--- _static/docs.js
+++ _static/docs.js
@@ -1,8 +1,51 @@
+/* front page */
 (function() {
   $(document).ready(function() {
-    $lc = $('.literally').literallycanvas({
+    $('.literally.index').literallycanvas({
       backgroundColor: 'whiteSmoke',
-      imageURLPrefix: '_static/lib/img'
+      imageURLPrefix: '/_static/lib/img'
+    });
+  });
+}).call(this);
+
+/* imgur */
+(function() {
+  $(document).ready(function() {
+    $('.literally.imgur').literallycanvas({
+      backgroundColor: 'whiteSmoke',
+      imageURLPrefix: '/_static/lib/img',
+      onInit: function(lc) {
+
+        $('a[data-action=upload-to-imgur]').click(function(e) {
+          localStorage.doUpload = true;
+          localStorage.imageBase64 = lc.canvasForExport().toDataURL().split(',')[1];
+        })
+
+        var extractToken = function(hash) {
+          var match = hash.match(/access_token=(\w+)/);
+          return !!match && match[1];
+        };
+
+        var token = extractToken(document.location.hash);
+        console.log(token, localStorage.doUpload);
+        if (token && JSON.parse(localStorage.doUpload)) {
+          localStorage.doUpload = false;
+          console.log("do upload");
+          $.ajax({
+            url: 'https://api.imgur.com/3/image',
+            method: 'POST',
+            headers: {
+              Authorization: 'Bearer ' + token,
+              Accept: 'application/json'
+            },
+            data: {image: localStorage.imageBase64, type: 'base64'},
+            success: function(result) {
+              var id = result.data.id;
+              window.location = 'https://imgur.com/gallery/' + id;
+            }
+          });
+        }
+      }
     });
   });
 }).call(this);
