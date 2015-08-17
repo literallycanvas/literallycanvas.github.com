6606a5beafad90705f028d7cc5da36a7ff5499f2
Steve Johnson <steve@graffitilabs.com>
2015-08-16 18:55:52 -0700

    Titles are my life

--- conf.py
+++ conf.py
@@ -53,7 +53,7 @@ html_sidebars = {
         'globaltoc.html', 'sidebarhelp.html', 'searchbox.html'],
 }
 
-html_title = "%(project)s v%(release)s docs - JS/Canvas drawing board - MS Paint in your web browser" % {
+html_title = "%(project)s v%(release)s docs - HTML5 drawing board - MS Paint in your web browser" % {
     'project': project, 'release': release}
 html_short_title = "Home"
 
